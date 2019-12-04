0a1045e1d2eee64422d8f8e15606d77a40d75253
xxmawhu <xxmawhu@163.com>
2019-12-04 14:10:41 +0800

    update

---
+++ TupleSvc/TupleSvc.h
@@ -0,0 +1,21 @@
+/* ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Xin-Xin MA
+#   Email         : xxmawhu@163.com
+#   File Name     : TupleSvc.h
+#   Create Time   : 2019-12-04 14:09
+#   Last Modified : 2019-12-04 14:09
+#   Describe      :
+#
+# ====================================================*/
+#ifndef  _TUPLESVC_H
+#define  _TUPLESVC_H
+
+class TupleSvc{
+    TupleSvc();
+}
+
+#endif // _TUPLESVC_H
+
+
