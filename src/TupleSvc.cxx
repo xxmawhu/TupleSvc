2339a89b046067d44ae90566692b635b590c0fdd
xxmawhu <xxmawhu@163.com>
2019-12-13 20:19:10 +0800

    update

--- src/TupleSvc.cxx
+++ src/TupleSvc.cxx
@@ -31,22 +31,22 @@ TupleSvc::TupleSvc() : m_intItemItr(0), m_doubleItemItr(0), m_arrayItemItr(0) {
 TupleSvc::~TupleSvc() {}
 
 void TupleSvc::SetItemAddress(const string& type_name,
-                              NTuple::Item<double>* m_tuple,
-                              const int& max_length) {
+        NTuple::Item<double>* m_tuple,
+        const int& max_length) {
     m_doubleItemItr = m_tuple;
     m_doubleLength = max_length;
 }
 
 void TupleSvc::SetItemAddress(const string& type_name,
-                              NTuple::Item<int>* m_tuple,
-                              const int& max_length) {
+        NTuple::Item<int>* m_tuple,
+        const int& max_length) {
     m_intItemItr = m_tuple;
     m_intLength = max_length;
 }
 
 void TupleSvc::SetArrayAddress(const string& type_name,
-                               NTuple::Array<double>* m_tuple,
-                               const int& max_length) {
+        NTuple::Array<double>* m_tuple,
+        const int& max_length) {
     m_arrayItemItr = m_tuple;
     m_arrayLength = max_length;
 }
@@ -84,14 +84,14 @@ bool TupleSvc::InitTuple(NTuple::Tuple* tuple) {
             ++itr){
         tuple->addItem(itr->first, m_intItemItr[itr->second]);
     }
-    
+
     // add double
     for(map<string, int>::iterator itr=m_doubleItemIndex.begin();
             itr!= m_doubleItemIndex.end();
             ++itr){
         tuple->addItem(itr->first, m_doubleItemItr[itr->second]);
     }
-    
+
     // add p4
     for(map<string, int>::iterator itr=m_ArrayIndex.begin();
             itr!= m_ArrayIndex.end();
@@ -132,22 +132,62 @@ void TupleSvc::InitItemIndex() {
         }
         const vector<string>& doubleList = avaiableInfo.GetDoubleInf();
         for (vector<string>::const_iterator itr = doubleList.begin();
-             itr != doubleList.end(); ++itr) {
+                itr != doubleList.end(); ++itr) {
             m_doubleItemIndex.insert(
-                pair<string, int>(name + (*itr), double_tmp_index++));
+                    pair<string, int>(name + (*itr), double_tmp_index++));
         }
         const vector<string>& intList = avaiableInfo.GetIntInf();
         for (vector<string>::const_iterator itr = intList.begin();
-             itr != intList.end(); ++itr) {
+                itr != intList.end(); ++itr) {
             m_intItemIndex.insert(
-                pair<string, int>(name + (*itr), int_tmp_index++));
+                    pair<string, int>(name + (*itr), int_tmp_index++));
         }
         const vector<string>& arrayList = avaiableInfo.GetP4Inf();
         for (vector<string>::const_iterator itr = arrayList.begin();
-             itr != arrayList.end(); ++itr) {
+                itr != arrayList.end(); ++itr) {
             m_ArrayIndex.insert(
-                pair<string, int>(name + (*itr), array_tmp_index));
+                    pair<string, int>(name + (*itr), array_tmp_index));
         }
     }
 }
 
+void TupleSvc::Fill(const string& particle_name, 
+        const string& observable,
+        const int& value){
+    int i  =  m_intItemIndex[particle_name+observable];
+    m_intItemItr[i] = value;
+}
+
+
+void TupleSvc::Fill(const string& particle_name, 
+        const string& observable,
+        const double& value){
+    int i  =  m_doubleItemIndex[particle_name+observable];
+    m_doubleItemItr[i] = value;
+}
+
+
+void TupleSvc::Fill(const string& particle_name, 
+        const string& observable,
+        const HepLorentzVector& p4){
+    int index  =  m_ArrayIndex[particle_name+observable];
+    for(int i=0; i< 4; ++i ) {
+        m_arrayItemItr[index][i] = p4[i];
+    }
+}
+
+
+void TupleSvc::Fill(const int& particle_index, const string& observable,
+        const double& value){
+    Fill(m_decayMode.ParicleName(particle_index), observable, value);
+}
+void TupleSvc::Fill(const int& particle_index, const string& observable,
+        const int& value){
+    Fill(m_decayMode.ParicleName(particle_index), observable, value);
+
+}
+void TupleSvc::Fill(const int& particle_index, const string& observable,
+        const HepLorentzVector& p4){
+    Fill(m_decayMode.ParicleName(particle_index), observable, p4);
+
+}
